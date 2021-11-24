42692fe28a42e1af1db1f857017b919134c1850f
besmarsh <ben.marsh555@googlemail.com>
2021-11-24 10:54:27 +0000

    Update Link to hold source and dest elements, remove Coord (not needed now Link changed and Element created

--- src/embs_design_contest/Link.java
+++ src/embs_design_contest/Link.java
@@ -2,17 +2,14 @@ package embs_design_contest;
 
 public class Link {
 
-    /*
-    * Communication object
-        * XY routing
-        * Outputs list of links used in routing
-    */
+    int source, dest;
 
-
-    Coord c;
-
-
-    Link(long x, long y){
-        c = new Coord(x, y);
+    Link(int x, int y){
+        source = x;
+        dest = y;
+    }
+    
+    public String toString() {
+		return "Link from " + source + " to " + dest;
     }
 }
